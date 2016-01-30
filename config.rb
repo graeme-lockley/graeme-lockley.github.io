6a516d51eeb396beea85bb73089cf7ece3d93f57
graemel <graemel@no9.co.za>
2016-01-30 16:09:55 +0200

    Added github deploy settings

--- config.rb
+++ config.rb
@@ -13,12 +13,12 @@ activate :automatic_image_sizes
 activate :syntax # code highlighting
 
 # github userpages deploy
-#activate :deploy do |deploy|
-  #deploy.method = :git
-  #deploy.build_before = true
-  #deploy.branch   = "master"
-  #deploy.remote   = "git@github.com:username/username.github.io.git"
-#end
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+  deploy.branch   = "master"
+  deploy.remote   = "https://github.com/graeme-lockley/graeme-lockley.github.io.git"
+end
 
 # github project pages deploy
 #activate :deploy do |deploy|
